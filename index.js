8b297b9f9d30a7b08e4cef135797fb05c9f444c0
Furqan Hameed <furqancode@gmail.com>
2023-03-29 22:55:20 +0530

    Updated content

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 const content = `
-    Hello ....
-
-    It me Furqan Hameed
+🐙 Github : https://github.com/Furqaaan
+🐳 Linkedin : https://linkedin.com/in/Furqaaan
+🦖 Leetcode: https://leetcode.com/Furqaaan
 `;
 
 console.log(content);
