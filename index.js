125ee556228ccb3d4485c0012406ec34d18fb8c7
Furqan Hameed <furqancode@gmail.com>
2023-03-30 20:24:07 +0530

    Added console clear before running the app

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
 
+const clearConsole = require("clear-any-console");
+
 const content = `
 🐳 Linkedin : https://linkedin.com/in/Furqaaan
 🐙 Github   : https://github.com/Furqaaan
 🦖 Leetcode : https://leetcode.com/Furqaaan
 `;
+
+// To clear everything in terminal.
+clearConsole();
 
 console.log(content);
